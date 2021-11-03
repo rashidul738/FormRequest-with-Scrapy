18ed8e680332e333358475f9815c61ff7454b56e
rashidul738 <rashidul7713@gmail.com>
2021-11-03 09:50:12 +0600

    scrapy FormRequest

--- fromRequest/spiders/fromrequest.py
+++ fromRequest/spiders/fromrequest.py
@@ -1,10 +1,28 @@
 import scrapy
+from scrapy import FormRequest
+from scrapy.shell import inspect_response
 
 
 class FromrequestSpider(scrapy.Spider):
     name = 'fromrequest'
-    allowed_domains = ['x']
-    start_urls = ['http://x/']
+    allowed_domains = ['casablanca-bourse.com']
+    start_urls = ['https://www.casablanca-bourse.com/bourseweb/Societe-Cote.aspx?codeValeur=12200']
 
     def parse(self, response):
-        pass
+        headers = {'Content-Type': 'text/plain; charset=utf-8'}
+        fromRequest = {
+            'TopControl1$ScriptManager1': 'SocieteCotee1$UpdatePanel1|SocieteCotee1$LBFicheTech',
+            '__EVENTTARGET': 'SocieteCotee1$LBFicheTech',
+            'TopControl1$TxtRecherche': '',
+            'TopControl1$txtValeur': '',
+            'SocieteCotee1$ListScCote': '',
+            'hiddenInputToUpdateATBuffer_CommonToolkitScripts': '1'
+        }
+        
+        yield FormRequest.from_response(response,
+                                  formdata=fromRequest,
+                                  headers=headers,
+                                  callback=self.parse_form)
+        
+    def parse_form(self, response):
+        print(response.css('#SocieteCotee1_FicheTechnique1_lblISIN ::text').get())    
